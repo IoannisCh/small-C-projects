65242f171d67c4393d934e805d3b4918c045e1b9
Ioannis Chantolios <ioannis.chantolios@ieee.org>
2020-06-19 22:31:43 +0100

    adding projects

---
+++ month.c
@@ -0,0 +1,23 @@
+/*
+ Author:Ioannis_Ch
+ Date:03/06/2020
+ Purpose:Training
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MONTHS 12
+
+int main()
+{
+	int days[MONTHS] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int index;
+
+    for (index = 0; index < MONTHS; index++)
+        printf("Month %d has %2d days. \n", index +1, days[index]);
+    
+    
+	return 0;
+
+}
